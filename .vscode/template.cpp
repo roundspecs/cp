aecf0b58443c892d69a6f6aab81ac5f7bf9262e3
Zarif <2502.zarif@gmail.com>
2022-09-23 18:04:34 +0600

    meh

--- .vscode/template.cpp
+++ .vscode/template.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll       long long
-#define vi       vector<int>
-#define pi       pair<int, int>
-#define fo(i, n) for (int i = 0; i < n; i++)
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef set<int> si;
 #define nn       "\n"
 #define mod      1000000007
-#define all(var) var.begin(), var.end()
+#define fo(i, n) for (int i = 0; i < n; i++)
 
 int main() {
-
+  
   return 0;
 }
