b68d59765c251f264ee87b7a24845a894b15dd89
Zarif <2502.zarif@gmail.com>
2022-10-14 14:32:14 +0600

    final

--- C/main.cpp
+++ C/main.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 #define fo(i, n) for (int i = 0; i < n; i++)
-#define nn       '\n'
+#define nn '\n'
+#define fast                                                                   \
+  ios_base::sync_with_stdio(0);                                                \
+  cin.tie(0)
+typedef long long ll;
 using namespace std;
 
-int main() {
-  
+signed main() {
+  fast;
+  int t;
+  cin >> t;
+  while (t--) {
+    
+  done:
+    cout << nn;
+  }
 }
