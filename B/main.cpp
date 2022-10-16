fbf858c0d2654ecb23d5f5eea2745edcb3601399
Zarif <2502.zarif@gmail.com>
2022-10-16 22:38:03 +0600

    remove trailing new line

--- B/main.cpp
+++ B/main.cpp
@@ -4,15 +4,13 @@
 typedef long long ll;
 using namespace std;
 void lesgo(int);
+
 signed main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   int t = 1;
-  // cin >> t;
-  fo(tc, t) {
-    lesgo(tc + 1);
-    cout << '\n';
-  }
+  cin >> t;
+  fo(tc, t) lesgo(tc + 1);
 }
 
 void lesgo(int tc) {
