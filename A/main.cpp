2b6ee4dc7ab9e37d58c0a0be99ff19e2351132b5
Zarif <2502.zarif@gmail.com>
2022-10-16 11:54:09 +0600

    lesgo

--- A/main.cpp
+++ A/main.cpp
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 #define fo(i, n) for (int i = 0; i < n; i++)
 #define nn '\n'
-#define fast                                                                   \
-  ios_base::sync_with_stdio(0);                                                \
-  cin.tie(0)
 typedef long long ll;
 using namespace std;
-
+void lesgo(int);
 signed main() {
-  fast;
-  int t;
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int t = 1;
   cin >> t;
-  while (t--) {
-    
-  done:
-    cout << nn;
+  fo(tc, t) {
+    lesgo(tc + 1);
+    cout << '\n';
   }
+}
+
+void lesgo(int tc) {
+  
 }
