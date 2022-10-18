9eec530d3a8b580afd4490e3c4baa9fa406d05e1
Zarif <2502.zarif@gmail.com>
2022-10-19 05:59:47 +0600

    reduce formatting

--- B/main.cpp
+++ B/main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
-#define fo(i, n) for (int i = 0; i < n; i++)
+#define fo(i,n) for(int i=0;i<n;i++)
 #define nn '\n'
+#define vi vector<int>
 typedef long long ll;
 using namespace std;
 void lesgo(int);
@@ -8,9 +9,9 @@ void lesgo(int);
 signed main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
-  int t = 1;
-  cin >> t;
-  fo(tc, t) lesgo(tc + 1);
+  int t=1;
+  cin>>t;
+  fo(tc,t) lesgo(tc+1);
 }
 
 void lesgo(int tc) {
