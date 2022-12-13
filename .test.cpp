2f4d1eb14d347acdf61861134dfb6eb03fcf0807
roundspecs <2502.zarif@gmail.com>
2022-12-13 11:19:03 +0600

    change tab size

--- .test.cpp
+++ .test.cpp
@@ -8,11 +8,11 @@ void lesgo();
 int t=1,tc;
 
 int32_t main() {
-  ios_base::sync_with_stdio(0); cin.tie(0);
-  cin>>t;
-  while(tc++ < t) lesgo();
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin>>t;
+    while(tc++ < t) lesgo();
 }
 
 void lesgo() {
-  
+    
 }
