884baf705a5089bfa7ff51d5b2cf3506f7c7df08
roundspecs <2502.zarif@gmail.com>
2022-12-22 16:34:41 +0600

    new template

--- .test.cpp
+++ .test.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
-#define f(i,s,n) for(int i=s; i<n; i++)
+#define f(i,s,n) for(int i=s;i<n;i++)
 #define nn '\n';
+#define all(x) begin(x),end(x)
+#define fast ios_base::sync_with_stdio(0);cin.tie(0)
 using namespace std;
 using ll=long long;
 using vi=vector<int>;
-void lesgo();
-int t=1,tc;
+int nxt(){int x;cin>>x;return x;}
 
 int32_t main() {
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    cin>>t;
-    while(tc++ < t) lesgo();
-}
+	fast;
+	int t=1;
+	// cin>>t;
+	f(tc,1,t+1) {
 
-void lesgo() {
-    
+	}
 }
