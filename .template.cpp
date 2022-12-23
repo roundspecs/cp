bdcf30814a296c5b2e4c90d9e48960f5c70cfd7f
roundspecs <2502.zarif@gmail.com>
2022-12-23 10:21:37 +0600

    return of the lesgo

--- .template.cpp
+++ .template.cpp
@@ -2,17 +2,18 @@
 #define f(i,s,n) for(int i=s;i<n;i++)
 #define nn '\n';
 #define all(x) begin(x),end(x)
-#define fast ios_base::sync_with_stdio(0);cin.tie(0)
 using namespace std;
 using ll=long long;
 using vi=vector<int>;
 int nxt(){int x;cin>>x;return x;}
+int tc,t=1;
+
+void lesgo() {
+
+}
 
 int32_t main() {
-	fast;
-	int t=1;
-	// cin>>t;
-	f(tc,1,t+1) {
-		
-	}
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin>>t;
+	while(tc++ < t) lesgo();
 }
