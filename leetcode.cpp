126f28c03ed916994d24fe06df7c8cd10feed4c4
roundspecs <2502.zarif@gmail.com>
2022-12-26 06:04:32 +0600

    add leetcode support

---
+++ leetcode.cpp
@@ -0,0 +1,12 @@
+#include <leetcode.h>
+
+#define f(i, s, n) for (int i = s; i < n; i++)
+#define all(x) begin(x), end(x)
+using ll = long long;
+using vi = vector<int>;
+
+
+
+int main() {
+  output(1);
+}
