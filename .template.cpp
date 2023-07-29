751c50bc49750cf12b8010db47fe21ba319c7a12
roundspecs <roundspecs@pm.me>
2023-07-30 00:56:58 +0600

    Add template to test

--- .template.cpp
+++ .template.cpp
@@ -10,4 +10,5 @@ using namespace std;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
+
 }
---
+++ .test.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file template.cpp
+ * @author roundspecs (roundspecs@pm.me)
+ * @brief Template for competitive programming
+ * @version 1.0
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+}
