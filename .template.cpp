cdd2f38ecab62a25406e84237b6bc04d5f7f5712
roundspecs <2502.zarif@gmail.com>
2023-11-29 19:24:30 +0600

    remove advices

--- .template.cpp
+++ .template.cpp
@@ -6,21 +6,3 @@ int main() {
   cin.exceptions(cin.failbit);
 
 }
-
-
-
-
-
-
-
-
-
-
-/**
- * DP: ready goes with dp
- * BITMASK: x = a ^ b ^ x
- * GRAPH: use vis
- * * DFS: use at, nxt
- * STYLE: use {} even for 1 line
- * EXIT: exit(0);
- */
---
+++ README.md
@@ -0,0 +1,8 @@
+# Advices
+
+DP: ready goes with dp
+BITMASK: x = a ^ b ^ x
+GRAPH: use vis
+DFS: use at, nxt
+STYLE: use {} even for 1 line
+EXIT: exit(0);
