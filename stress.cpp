641eb208fb528a9745daad186c0edfc85de2c13b
roundspecs <2502.zarif@gmail.com>
2023-12-11 11:38:32 +0600

    better stress

--- stress.cpp
+++ stress.cpp
@@ -4,8 +4,6 @@ using namespace std;
 int main() {
   random_device rd;
   uniform_int_distribution<int> tc(1,10);
-  uniform_int_distribution<int> INT(INT_MIN,INT_MAX);
-  uniform_int_distribution<int> UINT(0,INT_MAX);
   int T=tc(rd);
   cout<<T<<"\n";
   while(T--) {
