d4c573f19dedd72878ec13b3c9f2e057aa3da769
roundspecs <2502.zarif@gmail.com>
2023-12-31 16:44:42 +0600

    return of solve

--- .template.cpp
+++ .template.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void solve() {
+
+}
+
 int main() {
   cin.tie(0)->sync_with_stdio(0);
-
+  int T = 1;
+  cin >> T;
+  while (T--)
+    solve();
 }
