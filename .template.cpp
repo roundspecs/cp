bf8c3191f16e8d3a40aca96de9898e8326cdbf41
roundspecs <2502.zarif@gmail.com>
2024-01-28 18:34:37 +0600

    remove clutter

--- .template.cpp
+++ .template.cpp
@@ -1,20 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#define ordered_set                                       \
-  __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, \
-                   __gnu_pbds::rb_tree_tag,               \
-                   __gnu_pbds::tree_order_statistics_node_update>
-
-void solve() {
-
-}
-
 int main() {
   cin.tie(0)->sync_with_stdio(0);
-  int T = 1;
-  cin >> T;
-  while (T--) solve();
+  int t;
+  cin>>t;
+  while(t--) {
+    
+  }
 }
--- .test.cpp
+++ .test.cpp
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-
-}
-
 int main() {
   cin.tie(0)->sync_with_stdio(0);
-  int T = 1;
-  cin >> T;
-  while (T--)
-    solve();
+  int t;
+  cin>>t;
+  while(t--) {
+    
+  }
 }
