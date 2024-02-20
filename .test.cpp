d30a17c8e1c95605ff45475ad88df4b21e0072bc
roundspecs <2502.zarif@gmail.com>
2024-02-20 16:54:28 +0600

    add tc

--- .test.cpp
+++ .test.cpp
@@ -6,7 +6,8 @@ int main() {
     cin.tie(0)->sync_with_stdio(0);
     int T;
     cin >> T;
-    while (T--) {
+    for(int tc = 1; tc <= T; tc++) {
+        cout << "Case " << tc << ":\n";
         
     }
 }
