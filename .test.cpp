54d18e982f1748db24014edc9743f1bef9980c6d
roundspecs <2502.zarif@gmail.com>
2024-03-14 15:07:41 +0600

    update testcse

--- .test.cpp
+++ .test.cpp
@@ -6,8 +6,7 @@ int main() {
     cin.tie(0)->sync_with_stdio(0);
     int T;
     cin >> T;
-    for(int tc = 1; tc <= T; tc++) {
-        cout << "Case " << tc << ":\n";
+    fwhile(T--) {
         
     }
 }
