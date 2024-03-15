eb1c2e21e9307b31ba9cb93f17cfd83b3b496984
roundspecs <2502.zarif@gmail.com>
2024-03-15 08:44:30 +0600

    update debug template

--- .test.cpp
+++ .test.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define deb(...) 122
+#endif
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
